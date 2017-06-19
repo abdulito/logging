7f333ee0d433cdad2cdffbde898bff3601705689
Greg Banks <gbanks@gmail.com>
2017-06-19 16:02:17 -0700

    add "formatters" to streams.Template + tests

--- test/streams/TemplateTests.js
+++ test/streams/TemplateTests.js
@@ -1,6 +1,7 @@
 var assert = require('assert')
 
 var _ = require('lodash')
+var sinon = require('sinon')
 
 var __ = require('@carbon-io/fibers').__(module)
 var _o = require('@carbon-io/bond')._o(module)
@@ -16,7 +17,7 @@ __(function() {
     tests: [
       o({
         _type: testtube.Test,
-        name: 'SimpleLoggerWriteTest',
+        name: 'SimpleTemplateWriteTest',
         teardown: function() {
           carbonLog._reset()
         },
@@ -25,11 +26,85 @@ __(function() {
             name: 'foo',
             streams: [o({
               _type: carbonLog.streams.Template,
-              stream: new carbonLog.streams.StringIO()
+              stream: new carbonLog.streams.StringIO(false)
             })]
           })
           logger.info('foo')
           assert.equal(logger.streams[0]._stream.getValue(), 'foo')
+        }
+      }),
+      o({
+        _type: testtube.Test,
+        name: 'SimpleTemplateWriteCustomLogTemplateTest',
+        setup: function() {
+          this.sb = sinon.sandbox.create({
+            useFakeTimers: true
+          })
+        },
+        teardown: function() {
+          carbonLog._reset()
+        },
+        doTest: function() {
+          var logger = carbonLog.createLogger({
+            name: 'foo',
+            hostname: 'foo.bar',
+            streams: [o({
+              _type: carbonLog.streams.Template,
+              level: 'INFO',
+              template: '[{{time}}] <{{hostname}}> {{level}}: {{msg}}',
+              formatters: {
+                time: carbonLog.streams.Template.formatters.time.iso,
+                level: carbonLog.streams.Template.formatters.level.name
+              },
+              stream: new carbonLog.streams.StringIO(false)
+            })]
+          })
+          logger.info({foo: 'hello', bar: 'world'}, '{{foo}} {{bar}}')
+          assert.equal(
+            logger.streams[0]._stream.getValue(), 
+            '[1970-01-01T00:00:00.000Z] <foo.bar> INFO: hello world')
+          this.sb.clock.tick(1000)
+          logger.info({foo: 'hello', bar: 'world'}, '{{foo}} {{bar}}')
+          assert.equal(
+            logger.streams[0]._stream.getValue(), 
+            '[1970-01-01T00:00:01.000Z] <foo.bar> INFO: hello world')
+        }
+      }),
+      o({
+        _type: testtube.Test,
+        name: 'SimpleTemplateWriteCustomLogTemplateFormattersStringsTest',
+        setup: function() {
+          this.sb = sinon.sandbox.create({
+            useFakeTimers: true
+          })
+        },
+        teardown: function() {
+          carbonLog._reset()
+        },
+        doTest: function() {
+          var logger = carbonLog.createLogger({
+            name: 'foo',
+            hostname: 'foo.bar',
+            streams: [o({
+              _type: carbonLog.streams.Template,
+              level: 'INFO',
+              template: '[{{time}}] <{{hostname}}> {{level}}: {{msg}}',
+              formatters: {
+                time: 'time.iso',
+                level: 'level.name'
+              },
+              stream: new carbonLog.streams.StringIO(false)
+            })]
+          })
+          logger.info({foo: 'hello', bar: 'world'}, '{{foo}} {{bar}}')
+          assert.equal(
+            logger.streams[0]._stream.getValue(), 
+            '[1970-01-01T00:00:00.000Z] <foo.bar> INFO: hello world')
+          this.sb.clock.tick(1000)
+          logger.info({foo: 'hello', bar: 'world'}, '{{foo}} {{bar}}')
+          assert.equal(
+            logger.streams[0]._stream.getValue(), 
+            '[1970-01-01T00:00:01.000Z] <foo.bar> INFO: hello world')
         }
       })
     ]
