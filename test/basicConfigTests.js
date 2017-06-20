fc34a7edd7d9a6ca8b83b91dfc3636e74fffc0f3
Greg Banks <gbanks@gmail.com>
2017-06-20 13:31:40 -0700

    add basic configuration for "carbon-io.*"

--- test/basicConfigTests.js
+++ test/basicConfigTests.js
@@ -1,6 +1,7 @@
 var assert = require('assert')
 
 var _ = require('lodash')
+var sinon = require('sinon')
 
 var __ = require('@carbon-io/fibers').__(module)
 var _o = require('@carbon-io/bond')._o(module)
@@ -80,6 +81,42 @@ __(function() {
           assert.equal(logger1.level(), carbonLog.levels.TRACE)
           assert.equal(logger2.level(), carbonLog.levels.INFO)
           assert.equal(logger3.level(), carbonLog.levels.WARN)
+        }
+      }),
+      o({
+        _type: testtube.Test,
+        name: 'CarbonioBasicConfigTest',
+        setup: function() {
+          var self = this
+          this.sb = sinon.sandbox.create({
+            useFakeTimers: true
+          })
+          this.stderr = ''
+          this.sb.stub(process.stderr, 'write').callsFake(function(msg) {
+            self.stderr += msg
+          })
+        },
+        teardown: function() {
+          this.sb.restore()
+          carbonLog._reset()
+        },
+        doTest: function() {
+          var logger = carbonLog.createLogger({
+            name: 'carbon-io.foo',
+            hostname: 'foo'
+          })
+          assert.equal(logger.level(), carbonLog.levels.WARN)
+          logger.info('foo')
+          assert.equal(this.stderr, '')
+          carbonLog.configure({
+            'carbon-io.*' : {
+              level: 'INFO'
+            }
+          })
+          logger.info('foo')
+          assert.equal(
+            this.stderr, 
+            '[1970-01-01T00:00:00.000Z] <foo> INFO:carbon-io.foo: foo')
         }
       })
     ]
